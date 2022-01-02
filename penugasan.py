86bb4b35c4c0aac12a84e99294c68f72259e5db6
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-01-02 12:37:29 +0700

    assalamualaikum

---
+++ penugasan.py
@@ -0,0 +1,47 @@
+#belajar operator penugasan
+
+angka1 = 6
+angka2 = 4
+angka3 = angka1 + 2
+angka4 = angka1 - angka2
+angka5 = angka4 + angka4 + angka1
+angka6 = (angka4 + angka5) * angka2
+
+print("isi variabel angka1:",angka1)
+print("isi variabel angka2:",angka2)
+print("isi variabel angka3:",angka3)
+print("isi variabel angka4:",angka4)
+print("isi variabel angka5:",angka5)
+print("isi variabel angka6:",angka6)
+
+print("="*30)
+
+data = 20
+data += 10
+print("data += 10 :",data)
+
+data = 10
+data /= 2
+print("datA /= 2 :",data)
+
+data = 5
+data -= 3
+print("data -= 3 :",data)
+
+data = 6
+data *= 4
+print("data *= 4 :",data)
+
+data = 40
+data %= 10
+print("data %= 10 :",data)
+
+data = 5
+data **= 2
+print("data **= 2 :",data)
+
+data = 4
+data //= 2
+print("data //= 2 :",data)
+
+
